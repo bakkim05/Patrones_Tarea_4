d078ee34b61988bc5e4b66a2ec331728696f87a5
Jung <bakkim05@gmail.com>
2020-06-04 13:39:11 -0600

    pruebas

--- svm.py
+++ svm.py
@@ -5,15 +5,18 @@ Train a SVM to categorize 28x28 pixel images into digits (MNIST dataset).
 """
 
 import numpy as np
-from sklearn.datasets import load_digits
-from sklearn.utils import shuffle
 from sklearn.datasets import fetch_openml
 from sklearn.utils import shuffle
 from sklearn.svm import SVC
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from joblib import dump, load
-import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import roc_curve
+from sklearn.metrics import RocCurveDisplay
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import PrecisionRecallDisplay
 
 
 def main():
@@ -21,20 +24,20 @@ def main():
     data = get_data()
 
     
-    #clf = SVC(probability=False, kernel="rbf", C=1.8, gamma=0.001)
-    clf = SVC(probability=False, kernel="sigmoid", C=1.8, gamma=0.001)
-    #clf = SVC(probability=False, kernel="linear", C=1.8, gamma=0.001)
+    clf = SVC(probability=False, kernel="rbf", C=1.8, gamma=0.0073)
+    #clf = SVC(probability=False, kernel="sigmoid", C=1.8, gamma=0.0073)
+    #clf = SVC(probability=False, kernel="linear", C=1.8, gamma=0.0073)
 
     print("FITTING DATA...")
     
     examples = len(data["train"]["X"])
     clf.fit(data["train"]["X"][:examples], data["train"]["y"][:examples])
-    dump(clf, 'sigmoid-001-18.joblib')
-    #clf = load('rbf-073-28.joblib')
+    dump(clf, 'linear-073-28.joblib')
+    #clf = load('linear-073-28.joblib')
 
 
     print("ANALYZING DATA...")
-    #analyze(clf, data)
+    analyze(clf, data)
 
 
 def analyze(clf, data):
@@ -49,23 +52,30 @@ def analyze(clf, data):
     print("F1: %0.4f" % metrics.f1_score(y_test, predicted,average='weighted'))
 
     #Confusion Matrix
-    #cm = metrics.confusion_matrix(y_test, predicted)
-    #cm_display = metrics.ConfusionMatrixDisplay(cm).plot()
+    cm = confusion_matrix(data["test"]["y"], predicted)
+    cm_display = ConfusionMatrixDisplay(cm)
+    cm_display.plot()
+    show()
+    
 
     #ROC Curve
-    #fpr, tpr, _ = metrics.roc_curve(y_test, predicted, pos_label=clf.classes_[1])
-    #roc_display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr).plot()
+##    fpr, tpr, _ = roc_curve(y_test, predicted, pos_label=clf.classes_[1])
+##    roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr)
+##    roc_display.plot()
+##    show()
 
     #Precision Recall
-    #prec, recall, _ = metrics.precision_recall_curve(y_test, predicted, pos_label = clf.classes_[1])
-    #pr_display = metrics.PrecisionRecallDisplay(precision=prec, recall=recall).plot()
+##    prec, recall, _ = precision_recall_curve(y_test, predicted, pos_label = clf.classes_[1])
+##    pr_display = PrecisionRecallDisplay(precision=prec, recall=recall)
+##    pr_display.plot()
+##    show()
 
 
 
-    #fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(12,8))
-    #cm_display.plot(ax=ax1)
-    #roc_display.plot(ax=ax2)
-    #pr_display.plot(ax=ax3
+##    fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(12,8))
+##    cm_display.plot(ax=ax1)
+##    roc_display.plot(ax=ax2)
+##    pr_display.plot(ax=ax3)
 
 
 
